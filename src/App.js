9b8c623470aef6e5292936ce089f600c39131b9e
Elvin Hwang <hosung0519@hotmail.com>
2020-10-14 01:00:07 -0700

    pushed some changes

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import style from 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/Audio-Mixer">
             <div className="App">
                 <Navigation />
                 <div className="App-header">
                     <Switch>
-                        <Route path="/Audio-Mixer" component={Commands} exact />
-                        <Route path="/Audio-Mixer/dsl" component={DSLPage} />
-                        <Route path="/Audio-Mixer/commands" component={Commands} />
+                        <Route path="/" component={Commands} exact />
+                        <Route path="/dsl" component={DSLPage} exact />
+                        <Route path="/commands" component={Commands} exact/>
                     </Switch>
                 </div>
             </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,14 +6,18 @@ export class Navigation extends Component {
     render() {
         return (
             <Navbar bg="info" expand="lg">
-                <Navbar.Brand className="text-white" href="/">
+                <Navbar.Brand className="text-white" href="/Audio-Mixer">
                     SOUNDAPP
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
                     <Nav>
-                        <Nav.Link className="d-inline p-2 text-white" href="/Audio-Mixer/commands">Commands</Nav.Link>
-                        <Nav.Link className="d-inline p-2 text-white" href="/Audio-Mixer/dsl">Our DSL</Nav.Link>
+                        <Nav.Link className="d-inline p-2 text-white" href="/Audio-Mixer/commands">
+                            Commands
+                        </Nav.Link>
+                        <Nav.Link className="d-inline p-2 text-white" href="/Audio-Mixer/dsl">
+                            Our DSL
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
